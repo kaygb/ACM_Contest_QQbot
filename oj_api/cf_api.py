9efc32975a9dc5b541005179fdd1ab1dd559a7c4
INGg <1095490883@qq.com>
2022-07-22 14:09:32 +0800

    随机cf逻辑改回原先版本

--- oj_api/cf_api.py
+++ oj_api/cf_api.py
@@ -141,43 +141,28 @@ class CF(Contest):
             contest_list_all = list(json_data['result'])
             for contest in contest_list_all:
 
-                all_contest_list = []
-                edu_list = []
-                div1_list = []
-                div2_list = []
-                div3_list = []
-                div4_list = []
-
                 if (contest['type'] == 'CF' or contest['type'] == 'ICPC') and 'Codeforces' in contest[
                     'name']:  # 筛选edu，不带时间
                     if 'Educational Codeforces Round' in contest['name']:
-                        edu_list.append(contest)
+                        self.edu_list.append(contest)
 
                 if contest['relativeTimeSeconds'] > 0 and int(
                         time.time()) - 3 * 365 * 24 * 3600 <= contest['startTimeSeconds'] <= int(
                     time.time()) - 180 * 24 * 3600:  # 3年前到180天前
                     if (contest['type'] == 'CF' or contest['type'] == 'ICPC') and 'Codeforces' in contest[
                         'name']:  # 筛选常规的
-                        all_contest_list.append(contest)
+                        self.all_contest_list.append(contest)
                         if 'Div. 3' in contest['name']:
-                            div3_list.append(contest)
+                            self.div3_list.append(contest)
                         elif 'Div. 1 + Div. 2' in contest['name']:
-                            div1_list.append(contest)
-                            div2_list.append(contest)
+                            self.div1_list.append(contest)
+                            self.div2_list.append(contest)
                         elif 'Div. 2' in contest['name']:
-                            div2_list.append(contest)
+                            self.div2_list.append(contest)
                         elif 'Div. 1' in contest['name']:
-                            div1_list.append(contest)
+                            self.div1_list.append(contest)
                         elif 'Div. 4' in contest['name']:
-                            div4_list.append(contest)
-
-                self.all_contest_list = all_contest_list
-                self.edu_list = edu_list
-                self.div1_list = div1_list
-                self.div2_list = div2_list
-                self.div3_list = div3_list
-                self.div4_list = div4_list
-
+                            self.div4_list.append(contest)
 
     async def get_random_contest(self, type='normal'):
         if type == 'normal':
@@ -218,7 +203,7 @@ if __name__ == '__main__':
     #     print(asyncio.run(get_usr_rating(name)))
 
     cf = CF()
-    pprint.pprint((cf.list))
+    pprint.pprint(cf.div3_list)
     # logger.info(asyncio.run(cf.get_random_contest()))
     # logger.info(cf.contest_finshed_list)
     # pprint.pprint(cf.div1_list)
